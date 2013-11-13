605240f3a8e1ad1969f4e81ce2f49de4885aafdf
Nick Wynja <nick@nickwynja.com>
2013-11-12 22:42:30 -0500

    Adds track table migration

--- updates/1.0.1.sh
+++ updates/1.0.1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "This is an example of running and update for 1.0.1"
+STATEMENT="CREATE TABLE vs_track (  id int(11) unsigned NOT NULL AUTO_INCREMENT,  url varchar(120) DEFAULT NULL,  time_viewed datetime DEFAULT NULL,  PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+sudo mysql -D vspi_local -e "$STATEMENT"
+echo "Added 'vs_track' table to 'vspi_local' database"
